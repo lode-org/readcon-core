94fb2bfeaffbaf1b6149b1db06960e1a56e0c71f
Rohit Goswami <rog32@hi.is>
2025-06-09 23:15:00 +0200

    MAINT: Actually link up error struct

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,20 @@
+use std::num::{ParseFloatError, ParseIntError};
 #[derive(Debug)]
 pub enum ParseError {
     IncompleteHeader,
     IncompleteFrame,
     InvalidVectorLength { expected: usize, found: usize },
     InvalidNumberFormat(String),
+}
+
+impl From<ParseFloatError> for ParseError {
+    fn from(e: ParseFloatError) -> Self {
+        ParseError::InvalidNumberFormat(e.to_string())
+    }
+}
+
+impl From<ParseIntError> for ParseError {
+    fn from(e: ParseIntError) -> Self {
+        ParseError::InvalidNumberFormat(e.to_string())
+    }
 }
