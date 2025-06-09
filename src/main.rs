74c25fb1ecb6450b1a11ac16e6dd4b9885c64472
Rohit Goswami <rog32@hi.is>
2025-06-10 00:19:15 +0200

    ENH: Add in first working CLI

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,33 @@ fn main() {
         eprintln!("Error: File not found at {}", fname.display());
     }
     let fdat = fs::read_to_string(&args[1]).expect("Failed to read.");
-    let res = readcon::parser::parse_single_frame(&mut fdat.split("\n")).unwrap();
-    println!("{:?}", res);
+    let parser = readcon::iterators::ConFrameIterator::new(&fdat);
+    let good_frames: Vec<readcon::types::ConFrame> = parser
+        .filter_map(|result| match result {
+            Ok(frame) => Some(frame),
+            Err(e) => {
+                eprintln!(
+                    "-> Note: Discarding an incomplete or invalid frame. Error: {:?}\n",
+                    e
+                );
+                None
+            }
+        })
+        .collect();
+
+    println!(
+        "-> Parsing complete. Found {} valid frames.",
+        good_frames.len()
+    );
+
+    if let Some(last_frame) = good_frames.last() {
+        println!("\n-> Summary of last valid frame:");
+        println!("  - Box vectors: {:?}", last_frame.header.boxl);
+        println!("  - Angles: {:?}", last_frame.header.angles);
+        println!("  - Atom types: {}", last_frame.header.natm_types);
+        println!("  - Total atoms: {}", last_frame.atom_data.len());
+        if let Some(last_atom) = last_frame.atom_data.last() {
+            println!("  - Last atom: {:?}", last_atom);
+        }
+    }
 }
