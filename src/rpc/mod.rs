a221dbd8dc6f9e9ba96d7dce1f8199b2a6877a60
Rohit Goswami <rgoswami@ieee.org>
2026-08-20 04:59:39 -0500

    test(rpc): reject incompatible ABI majors

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -41,3 +41,25 @@ pub fn validate_compatibility_stamp(
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rejects_a_breaking_abi_major() {
+        let mut message = capnp::message::Builder::new_default();
+        {
+            let mut stamp = message.init_root::<compatibility_stamp::Builder>();
+            stamp.set_abi_major(2);
+            stamp.set_abi_minor(crate::ffi::RKR_ABI_VERSION_MINOR);
+            stamp.set_abi_layout_revision(crate::ffi::RKR_ABI_LAYOUT_REVISION);
+            stamp.set_con_spec_version(crate::CON_SPEC_VERSION);
+        }
+        let stamp = message
+            .get_root_as_reader::<compatibility_stamp::Reader>()
+            .expect("compatibility stamp root");
+        let error = validate_compatibility_stamp(stamp).expect_err("major mismatch must fail");
+        assert!(error.contains("ABI major"));
+    }
+}
