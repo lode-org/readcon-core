e7b16dc21090493bf5365091d1f530224578c5a7
Rohit Goswami <rgoswami@ieee.org>
2026-08-24 06:02:35 -0500

    test(rpc): reject incompatible ABI majors and newer minors

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -39,3 +39,42 @@ pub fn validate_compatibility_stamp(stamp: compatibility_stamp::Reader<'_>) -> R
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rejects_a_breaking_abi_major() {
+        let mut message = capnp::message::Builder::new_default();
+        {
+            let mut stamp = message.init_root::<compatibility_stamp::Builder>();
+            stamp.set_abi_major(2);
+            stamp.set_abi_minor(crate::ffi::RKR_ABI_VERSION_MINOR);
+            stamp.set_abi_layout_revision(crate::ffi::RKR_ABI_LAYOUT_REVISION);
+            stamp.set_con_spec_version(crate::CON_SPEC_VERSION);
+        }
+        let stamp = message
+            .get_root_as_reader::<compatibility_stamp::Reader>()
+            .expect("compatibility stamp root");
+        let error = validate_compatibility_stamp(stamp).expect_err("major mismatch must fail");
+        assert!(error.contains("ABI major"));
+    }
+
+    #[test]
+    fn rejects_a_newer_abi_minor() {
+        let mut message = capnp::message::Builder::new_default();
+        {
+            let mut stamp = message.init_root::<compatibility_stamp::Builder>();
+            stamp.set_abi_major(crate::ffi::RKR_ABI_VERSION_MAJOR);
+            stamp.set_abi_minor(crate::ffi::RKR_ABI_VERSION_MINOR + 1);
+            stamp.set_abi_layout_revision(crate::ffi::RKR_ABI_LAYOUT_REVISION);
+            stamp.set_con_spec_version(crate::CON_SPEC_VERSION);
+        }
+        let stamp = message
+            .get_root_as_reader::<compatibility_stamp::Reader>()
+            .expect("compatibility stamp root");
+        let error = validate_compatibility_stamp(stamp).expect_err("newer minor must fail");
+        assert!(error.contains("ABI minor"));
+    }
+}
